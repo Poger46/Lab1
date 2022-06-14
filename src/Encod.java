7cd41f164edbb0efa4b82f705289422a7f3029d5
Poger46 <53600012+Poger46@users.noreply.github.com>
2022-06-14 19:35:51 +0300

    Initial commit

---
+++ src/Encod.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+
+public class Encod {
+    public static void main(String[] args){
+
+        EncodingConverter ec = new EncodingConverter();
+        ec.EConverter("C:\\Users\\Poger46\\untitled7\\src\\input.txt","C:\\Users\\Poger46\\untitled7\\src\\output.txt", "utf8","utf16" );
+    }
+}
