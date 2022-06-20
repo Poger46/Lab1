9fd8629addd7991d125d050cf00bf78bea8ca015
Poger46 <53600012+Poger46@users.noreply.github.com>
2022-06-20 18:19:36 +0300

    1

--- src/Encod.java
+++ src/Encod.java
@@ -4,6 +4,6 @@ public class Encod {
     public static void main(String[] args){
 
         EncodingConverter ec = new EncodingConverter();
-        ec.EConverter("C:\\Users\\Poger46\\untitled7\\src\\input.txt","C:\\Users\\Poger46\\untitled7\\src\\output.txt", "utf8","utf16" );
+        ec.EConverter("C:\\Users\\Poger46\\untitled8\\src\\input.txt","C:\\Users\\Poger46\\untitled8\\src\\output.txt", "utf8","utf16" );
     }
 }
